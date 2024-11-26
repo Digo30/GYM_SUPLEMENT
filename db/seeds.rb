128688a0bc6b39259935dfe7b8067645b83b6c50
alvaro bernucci <alvarosenabernucci@hotmail.com>
2024-11-26 15:06:38 -0300

    Back de products, com validação

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,43 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# User.create!(email: 'teste@exemplo.com', password: 'senha123', password_confirmation: 'senha123')
+
+
+p "Criando Products"
+Product.create(
+  name: "Whey Protein",
+  price: 110.00,
+  stock: 5,
+  brand: "Integral Medica",
+  category: "Proteina",
+  user_id: 1
+)
+
+
+Product.create(
+  name: "Multivitaminico",
+  price: 50.00,
+  stock: 7,
+  brand: "Growth",
+  category: "Vitamina",
+  user_id: 1
+
+)
+
+Product.create(
+  name: "Creatina monohidratada",
+  price: 100.00,
+  stock: 5,
+  brand: "Integral Medica",
+  category: "Creatina",
+  user_id: 1
+
+)
+
+Product.create(
+  name: "Barra de proteína",
+  price: 20.00,
+  stock: 30,
+  brand: "Integral Medica",
+  category: "Proteina",
+  user_id: 1
+)
+p "Produtos criados"
