674880ce987f2248025737eb2a54116967f5fade
Rodrigo Silva <rocsdesigner@gmail.com>
2024-11-27 13:44:17 -0300

    Login update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   #Autenticantion login user
-  before_action :authenticate_user!
+  #before_action :authenticate_user!, QUANDO TIVER A PAGINA DE COMPRA ATIVAR ESTE CÓDIGO
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  #Autenticantion login user
-  skip_before_action :authenticate_user!, only: :home
+  #Autenticantion login user - Deixar comentado!!!!!!!
+  #skip_before_action :authenticate_user!, only: :home
 
   def home
     @products = Product.all
