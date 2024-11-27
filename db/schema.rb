5b5828c9219336a72d5ce226346854f7ba2a2287
Vinicius Neumann <v.ogell@web.de>
2024-11-27 16:05:34 -0300

    navbar functions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_171822) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_152424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_171822) do
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_checkouts_on_product_id"
     t.index ["user_id"], name: "index_checkouts_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "nome"
+    t.string "email"
+    t.text "mensagem"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
