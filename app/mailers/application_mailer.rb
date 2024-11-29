b00535d16bb71b199bf878f655c1ae66ce99be96
Gilmar Shuenck <gilmar_shuenck@hotmail.com>
2024-11-29 15:45:52 -0300

    bug heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,8 @@
-class ApplicationMailer 
-  default to: 'seu_email@dominio.com'
+class ApplicationMailer
+  # default to: 'seu_email@dominio.com'
 
-  def contact_email(contact)
-    @contact = contact
-    mail(from: @contact.email, subject: 'Mensagem de Contato')
-  end
+  # def contact_email(contact)
+  #   @contact = contact
+  #   mail(from: @contact.email, subject: 'Mensagem de Contato')
+  # end
 end
